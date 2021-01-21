2a65447dd2dcebb6376c70ba1d698e369f3bac40
Snehasish Dawn <snehasishdawn29@gmail.com>
2021-01-21 15:52:18 +0530

    added calculation

--- src/views/Fuel.vue
+++ src/views/Fuel.vue
@@ -9,38 +9,61 @@
                     Lap time
                 </div>
                 <div class="grid grid-cols-4 gap-2 items-center">
-                    <input type="text" class="dark:bg-gray-800 rounded p-1 text-center bg-gray-200">
+                    <input type="number" class="dark:bg-gray-800 rounded p-1 text-center bg-gray-200" min="0" max="60" v-model="lapTimeMin">
                     min
-                    <input type="text" class="dark:bg-gray-800 rounded p-1 text-center bg-gray-200">
+                    <input type="number" class="dark:bg-gray-800 rounded p-1 text-center bg-gray-200" min="0" max="60" v-model="lapTimeSec">
                     sec
                 </div>
                  <div class="text-xl col-span-1">
                     Stint Length
                 </div>
                 <div class="grid grid-cols-4 gap-2 items-center">
-                    <input type="text" class="dark:bg-gray-800 rounded p-1 text-center bg-gray-200">
+                    <input type="number" class="dark:bg-gray-800 rounded p-1 text-center bg-gray-200" v-model="stintLength">
                     min
                 </div>
                  <div class="text-xl col-span-1">
                     Litres per Lap
                 </div>
                 <div class="grid grid-cols-4 gap-2 items-center">
-                    <input type="text" class="dark:bg-gray-800 rounded p-1 text-center bg-gray-200">
+                    <input type="number" class="dark:bg-gray-800 rounded p-1 text-center bg-gray-200" v-model="litres">
                     L
                 </div>
             </div>
             <div class="grid grid-cols-2 gap-2 mt-4">
-                <div class="bg-gray-800 rounded-md text-center p-3">
+                <div class="dark:bg-gray-800 bg-gray-200 rounded-md text-center p-3">
                     Full Formation Lap
                 </div>
-                <div class="bg-gray-800 rounded-md text-center p-3">
+                <div class="dark:bg-gray-800 bg-gray-200 rounded-md text-center p-3">
                     Simple Formation Lap
                 </div>
             </div>
-            <div class="my-4 bg-gray-700 text-center p-4 rounded-lg hover:bg-gray-800 active:bg-black cursor-pointer select-none">
+            <div class="my-4 bg-blue-700 hover:bg-blue-800 text-center p-4 rounded-lg active:bg-black cursor-pointer select-none text-white font-semibold" @click="calculate()">
                 Calculate
             </div>
         </form>
-    
+        {{fuel}}
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            lapTimeMin: '',
+            lapTimeSec: '',
+            stintLength: '',
+            litres: '',
+            laptime: '',
+            fuel: '',
+            laps: '',
+        }
+    },
+    methods: {
+        calculate() {
+            this.laptime = parseInt(this.lapTimeMin*60)+parseInt(this.lapTimeSec);
+            this.laps = (parseInt(this.stintLength)*60)/parseInt(this.laptime);
+            this.fuel = Math.round(this.laps*(Number(this.litres)))
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,12 @@
       <div class="bg-gradient-to-r from-teal-400 to-blue-500 hover:from-blue-400 hover:to-teal-500 p-4 rounded-lg font-semibold cursor-pointer text-white text-center" @click="$router.push({name: 'Fuel'})">
         Fuel Calculator
       </div>
-      <div class="bg-gradient-to-r from-purple-400  to-red-500 hover:from-red-400 hover:to-purple-500 p-4 font-semibold rounded-lg cursor-pointer text-white text-center">
+      <!-- <div class="bg-gradient-to-r from-purple-400  to-red-500 hover:from-red-400 hover:to-purple-500 p-4 font-semibold rounded-lg cursor-pointer text-white text-center">
         Strategy
       </div>
       <div class="bg-gradient-to-r from-green-500 to-blue-400 hover:from-blue-500 hover:to-green-400 p-4 font-semibold rounded-lg cursor-pointer text-white text-center">
         Car Setup
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
